dac4bc0e3595d266f16f8ad80cb3c85b9467911b
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-06 23:39:47 -0600

    feat: 🍥 add new add liquidity script

---
+++ script/03_AddLiquidity.s.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { RemittanceCSMM } from "../src/RemittanceCSMM.sol";
+import { FFIHelper } from "./FFIHelper.sol";
+import { console } from "forge-std/Test.sol";
+import { IPoolManager } from "v4-core/interfaces/IPoolManager.sol";
+import { IERC20Minimal } from "v4-core/interfaces/external/IERC20Minimal.sol";
+import { LPFeeLibrary } from "v4-core/libraries/LPFeeLibrary.sol";
+import { Currency, CurrencyLibrary } from "v4-core/types/Currency.sol";
+import { PoolIdLibrary, PoolKey } from "v4-core/types/PoolKey.sol";
+
+address constant OWNER = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
+contract AddLiquidityScript is FFIHelper {
+
+  using CurrencyLibrary for Currency;
+  using PoolIdLibrary for PoolKey;
+
+  IPoolManager manager;
+  RemittanceCSMM hook;
+  bytes32 poolId;
+  Currency currency0;
+  Currency currency1;
+  PoolKey key;
+
+  function setUp() public {
+    manager = _getDeployedPoolManager();
+    hook = RemittanceCSMM(address(_getDeployedHook()));
+    uint256[] memory topics = _getPoolTopics();
+    poolId = bytes32(topics[1]);
+    currency0 = Currency.wrap(address(uint160(topics[2])));
+    currency1 = Currency.wrap(address(uint160(topics[3])));
+  }
+
+  function run() public {
+    key = PoolKey(currency0, currency1, LPFeeLibrary.DYNAMIC_FEE_FLAG, 60, hook);
+
+    vm.startBroadcast(OWNER);
+
+    uint256 amount = 100;
+
+    IERC20Minimal(Currency.unwrap(currency0)).approve(address(hook), amount);
+    IERC20Minimal(Currency.unwrap(currency1)).approve(address(hook), amount);
+
+    hook.addLiquidity(key, amount);
+
+    vm.stopBroadcast();
+  }
+
+}
