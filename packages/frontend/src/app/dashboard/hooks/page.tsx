64d9431eb2a89b87feab27c7a0b45bb2ca2b2e28
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-10 01:11:38 -0500

    feat: 🪝 add hooks page

---
+++ packages/frontend/src/app/dashboard/hooks/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { columns } from "./columns";
+import { DataTable } from "../data-table";
+import { fetchData, hooksQuery } from "@/lib/queries";
+
+export default function Hooks() {
+	const [hooks, setHooks] = useState([]);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const endpoint = "http://localhost:42069";
+		const fetchQueryData = async () => {
+			try {
+				fetchData(endpoint, hooksQuery).then(async (r) => {
+					const data = await r.json();
+					console.log(data);
+					setHooks(data.data.hooks.items);
+				});
+			} catch (err: any) {
+				setError(err);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchQueryData();
+	}, []);
+	return (
+		<div className="grid gap-4">
+			<h2>Hooks</h2>
+			<DataTable columns={columns} data={hooks} />
+		</div>
+	);
+}
