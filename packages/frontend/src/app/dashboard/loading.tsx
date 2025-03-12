7e01c9c4e16fe4f4204e286adf6be2cee2801b60
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-12 04:00:21 -0500

    feat: 🗝️ add keys to iterater elements

--- packages/frontend/src/app/dashboard/loading.tsx
+++ packages/frontend/src/app/dashboard/loading.tsx
@@ -8,17 +8,20 @@ export default function Loading() {
 			<div className="grid auto-rows-min gap-4 animate-pulse">
 				<Table>
 					<TableBody>
-						{Array(12)
+						{Array(50)
 							.fill(1)
 							.map((row, index) => (
-								<TableRow key={index} className="flex h-12 items-center gap-2">
+								<TableRow
+									key={Math.random()}
+									className="flex h-12 items-center gap-2"
+								>
 									{Array(5)
 										.fill(1)
-										.map((cell, index) => (
+										.map((cell) => (
 											<TableCell
-												key={index}
+												key={Math.random() + 1}
 												className="aspect-video gap-2 h-6 w-full rounded-xl bg-muted"
-											></TableCell>
+											/>
 										))}
 								</TableRow>
 							))}
