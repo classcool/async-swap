0c4617ea29c87c98d13d1b533ac9a1c07003b9cd
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-12 20:12:17 -0500

    chore: 👨🏽‍💻 remove login form

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import { CommandDialogDemo } from "@/components/command-menu";
-import { LoginForm } from "@/components/login-form";
 
 function App() {
 	return (
 		<div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
 			<CommandDialogDemo />
-			<div className="w-full max-w-sm md:max-w-3xl">
-				{/** <LoginForm /> */}
-			</div>
+			<div className="w-full max-w-sm md:max-w-3xl">{/** <LoginForm /> */}</div>
 		</div>
 	);
 }
