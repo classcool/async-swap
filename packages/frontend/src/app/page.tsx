c04bf87b4e8a7a514d52e0f34605dcd42d8e9879
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-12 19:42:55 -0500

    feat: ☁️ simplifiled home page

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
+import { CommandDialogDemo } from "@/components/command-menu";
 import { LoginForm } from "@/components/login-form";
 
 function App() {
 	return (
-		<div className="col-span-full bg-red-200">
-			<div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-				<div className="w-full max-w-sm md:max-w-3xl">
-					<LoginForm />
-				</div>
+		<div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+			<CommandDialogDemo />
+			<div className="w-full max-w-sm md:max-w-3xl">
+				{/** <LoginForm /> */}
 			</div>
 		</div>
 	);
