9d275321e75e68213782f7c6e554d51c8ccf0d63
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-27 20:11:10 -0500

    feat: add deploy shell scripts

---
+++ start_script.sh
@@ -0,0 +1,19 @@
+
+forge script --broadcast -f localhost:8545 --account anvil -vvvv script/00_DeployPoolManager.s.sol --password yes
+
+sleep 3
+
+forge script --broadcast -f localhost:8545 --account anvil -vvvv script/01_DeployHook.s.sol --password yes
+
+
+sleep 3
+
+forge script --broadcast -f localhost:8545 --account anvil -vvvv script/02_InitilizePool.s.sol --password yes
+
+sleep 3
+
+forge script --broadcast -f localhost:8545 --account anvil -vvvv script/03_AddLiquidity.s.sol --password yes
+
+sleep 3
+
+forge script --broadcast --rpc-url localhost:8545 --account anvil -vvvv script/04_Swap.s.sol --password yes
