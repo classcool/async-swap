19b455bb56ae7679678f180856a5a7fffceb0e55
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-27 20:08:40 -0500

    feat: add execute orders

--- src/CSMM.sol
+++ src/CSMM.sol
@@ -2,20 +2,26 @@
 pragma solidity 0.8.26;
 
 import { Router } from "./router.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { CurrencySettler } from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
+import { console } from "forge-std/Test.sol";
 import { IPoolManager } from "v4-core/interfaces/IPoolManager.sol";
 import { Hooks } from "v4-core/libraries/Hooks.sol";
+import { StateLibrary } from "v4-core/libraries/StateLibrary.sol";
+import { TransientStateLibrary } from "v4-core/libraries/TransientStateLibrary.sol";
 import { BalanceDelta, toBalanceDelta } from "v4-core/types/BalanceDelta.sol";
 import { BeforeSwapDelta, toBeforeSwapDelta } from "v4-core/types/BeforeSwapDelta.sol";
-import { Currency } from "v4-core/types/Currency.sol";
+import { Currency, CurrencyLibrary } from "v4-core/types/Currency.sol";
 import { PoolId } from "v4-core/types/PoolId.sol";
 import { PoolIdLibrary, PoolKey } from "v4-core/types/PoolKey.sol";
 import { BaseHook } from "v4-periphery/src/utils/BaseHook.sol";
 
 /// @title Remittance CSMM
 /// @notice A NoOp hook that mints 1:1 tokens
-contract CSMM is BaseHook {
+contract CSMM is Ownable, BaseHook {
 
+  using StateLibrary for IPoolManager;
+  using TransientStateLibrary for IPoolManager;
   using CurrencySettler for Currency;
   using PoolIdLibrary for PoolKey;
 
@@ -24,12 +30,23 @@ contract CSMM is BaseHook {
 
   event BeforeAddLiquidity(PoolId poolId, address sender, BalanceDelta liquidityDelta);
   event BeforeSwap(
-    bytes32 poolId, address owner, bool indexed zeroForOne, int256 indexed amountIn, uint256 indexed nonce
+    bytes32 poolId, address owner, bool indexed zeroForOne, int256 indexed amountIn, uint256 indexed nonc
   );
+
+  address asyncExecutor;
 
   error AddLiquidityThroughHook();
 
-  constructor(IPoolManager poolManager) BaseHook(poolManager) { }
+  constructor(IPoolManager poolManager) Ownable(msg.sender) BaseHook(poolManager) { }
+
+  modifier onlyAsyncExecutor() {
+    require(msg.sender == asyncExecutor);
+    _;
+  }
+
+  function setExecutor(address executor) public onlyOwner {
+    asyncExecutor = executor;
+  }
 
   function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
     return Hooks.Permissions({
@@ -59,19 +76,27 @@ contract CSMM is BaseHook {
     revert AddLiquidityThroughHook();
   }
 
-  struct CallbackData {
-    uint256 amountEach;
-    Currency currency0;
-    Currency currency1;
-    address sender;
-  }
-
   /// @notice Custom add liquidity function
   function addLiquidity(PoolKey calldata key, uint256 amountEach) external {
-    poolManager.unlock(abi.encode(CallbackData(amountEach, key.currency0, key.currency1, msg.sender)));
+    poolManager.unlock(
+      abi.encode(LiquidityCallback(ActionType.Liquidity, amountEach, key.currency0, key.currency1, msg.sender))
+    );
     emit BeforeAddLiquidity(
       key.toId(), msg.sender, toBalanceDelta(int128(int256(amountEach)), int128(int256(amountEach)))
     );
+  }
+
+  function executeOrders(PoolKey calldata key, AsyncOrder calldata order) external onlyAsyncExecutor {
+    Currency currency;
+    if (order.zeroForOne) {
+      currency = key.currency0;
+    } else {
+      currency = key.currency1;
+    }
+    uint256 claimable = asyncOrders[order.poolId][order.owner][order.zeroForOne];
+    if (claimable >= uint256(order.amountIn)) {
+      poolManager.transfer(order.owner, currency.toId(), uint256(order.amountIn));
+    }
   }
 
   struct AsyncOrder {
@@ -79,6 +104,11 @@ contract CSMM is BaseHook {
     address owner;
     bool zeroForOne;
     int256 amountIn;
+  }
+
+  struct TestSettings {
+    bool takeClaims;
+    bool settleUsingBurn;
   }
 
   /// @dev Async swap function
@@ -123,15 +153,65 @@ contract CSMM is BaseHook {
   /// @dev called by keeper to settle swaps and execute transation orders
   function settleAsyncSwap() external { }
 
+  enum ActionType {
+    Liquidity,
+    Swap
+  }
+
+  struct SwapSettings {
+    bool takeClaims;
+    bool settleUsingBurn;
+  }
+
+  struct SwapCallback {
+    ActionType action;
+    address sender;
+    SwapSettings swapSettings;
+    PoolKey key;
+    IPoolManager.SwapParams params;
+    bytes hookData;
+  }
+
+  struct LiquidityCallback {
+    ActionType action;
+    uint256 amountEach;
+    Currency currency0;
+    Currency currency1;
+    address sender;
+  }
+
+  function _handleLiquidityCallback(bytes memory rawData) internal onlyPoolManager {
+    LiquidityCallback memory data = abi.decode(rawData, (LiquidityCallback));
+    data.currency0.settle(poolManager, data.sender, data.amountEach, false);
+    data.currency1.settle(poolManager, data.sender, data.amountEach, false);
+
+    data.currency0.take(poolManager, address(this), data.amountEach, true);
+    data.currency1.take(poolManager, address(this), data.amountEach, true);
+  }
+
+  function _handleSwapCallback(bytes memory) internal view onlyPoolManager returns (bytes memory) {
+    // SwapCallback memory data = abi.decode(rawData, (SwapCallback));
+    // BalanceDelta delta = poolManager.swap(data.key, data.params, data.hookData);
+    // return abi.encode(delta);
+    return "";
+  }
+
   /// @notice callback from add liquidity.
-  function unlockCallback(bytes calldata data) external onlyPoolManager returns (bytes memory) {
-    CallbackData memory callbackData = abi.decode(data, (CallbackData));
+  function unlockCallback(bytes calldata data) external returns (bytes memory) {
+    bytes32 action;
+    assembly ("memory-safe") {
+      let loc := data.offset
+      let actionLoc := add(calldataload(loc), data.offset)
+      action := calldataload(actionLoc)
+    }
 
-    callbackData.currency0.settle(poolManager, callbackData.sender, callbackData.amountEach, false);
-    callbackData.currency1.settle(poolManager, callbackData.sender, callbackData.amountEach, false);
-
-    callbackData.currency0.take(poolManager, address(this), callbackData.amountEach, true);
-    callbackData.currency1.take(poolManager, address(this), callbackData.amountEach, true);
+    console.logBytes32(action);
+    if (action == bytes32(0)) {
+      _handleLiquidityCallback(data);
+    }
+    if (action == bytes32(uint256(1))) {
+      return _handleSwapCallback(data);
+    }
 
     return "";
   }
