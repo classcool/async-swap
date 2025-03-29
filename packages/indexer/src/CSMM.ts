5490faab9854cde3b9a85f4f89985a6213e0859b
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-29 01:11:08 -0500

    feat: add indexing on filled orders

--- packages/indexer/src/CSMM.ts
+++ packages/indexer/src/CSMM.ts
@@ -62,14 +62,43 @@ ponder.on("CsmmHook:BeforeSwap", async ({ event, context }) => {
 		.values({
 			chainId: context.network.chainId,
 			owner: event.args.owner,
-			nonce: event.args.nonce,
 			poolId: event.args.poolId,
 			zeroForOne: event.args.zeroForOne,
 			amountIn: event.args.amountIn,
 			timestamp: event.block.timestamp,
 		})
 		.onConflictDoUpdate((row) => ({
+		orderStatus: false,
 			amountIn: row.amountIn + event.args.amountIn,
 			timestamp: event.block.timestamp,
 		}));
+});
+
+ponder.on("CsmmHook:AsyncOrderFilled", async ({ event, context }) => {
+	await context.db
+		.insert(schema.order)
+		.values({
+			chainId: context.network.chainId,
+			poolId: event.args.poolId,
+			owner: event.args.owner,
+			zeroForOne: event.args.zeroForOne,
+			amountIn: event.args.amount,
+			timestamp: event.block.timestamp,
+			orderStatus: false,
+		})
+		.onConflictDoUpdate((row) => {
+			const orderDiff = row.amountIn - event.args.amount;
+			if (orderDiff === BigInt(0)) {
+				return {
+					amountIn: orderDiff,
+					orderStatus: true,
+					timestamp: event.block.timestamp,
+				};
+			}
+			return {
+				orderStatus: false,
+				amountIn: orderDiff,
+				timestamp: event.block.timestamp,
+			};
+		});
 });
