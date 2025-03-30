343d48178bc26157717b0144b41eedac218f09b0
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-30 16:27:39 -0500

    feat: add v2 assertions

--- test/AsyncCSMM.t.sol
+++ test/AsyncCSMM.t.sol
@@ -71,67 +71,60 @@ contract AsyncCsmmTest is SetupHook {
     assertEq(balance1Before, balance1After);
   }
 
-  function testFuzzAsyncSwapOrder(bool zeroForOne, int256 amount, bool settleUsingBurn) public userAction {
-    //   vm.assume(amount >= 1);
-    //   vm.assume(amount <= 1 ether);
-    //   vm.assume(settleUsingBurn == false);
-    //   // amount = 0xbeef;
-    //   // zeroForOne = false;
-    //   // settleUsingBurn = false;
-    //
-    //   uint256 balance0Before = manager.balanceOf(address(hook), currency0.toId());
-    //   uint256 balance1Before = manager.balanceOf(address(hook), currency0.toId());
-    //
-    //   uint256 userCurrency0Balance = currency0.balanceOf(user);
-    //   uint256 userCurrency1Balance = currency1.balanceOf(user);
-    //
-    //   IPoolManager.SwapParams memory params = IPoolManager.SwapParams({
-    //     zeroForOne: zeroForOne,
-    //     amountSpecified: -int256(amount),
-    //     sqrtPriceLimitX96: uint160(2 ** 96 + 1)
-    //   });
-    //   PoolSwapTest.TestSettings memory testSettings =
-    //     PoolSwapTest.TestSettings({ takeClaims: false, settleUsingBurn: settleUsingBurn });
-    //
-    //   AsyncCSMM.AsyncOrder memory order =
-    //     AsyncCSMM.AsyncOrder({ poolId: poolId, owner: user, zeroForOne: zeroForOne, amountIn: amount });
-    //   bytes memory hookData = abi.encode(order);
-    //
-    //   if (zeroForOne) {
-    //     token0.approve(address(router), uint256(amount));
-    //   } else {
-    //     token1.approve(address(router), uint256(amount));
-    //   }
-    //
-    //   router.swap(key, params, testSettings, hookData);
-    //
-    //   if (zeroForOne) {
-    //     assertEq(currency0.balanceOf(user), userCurrency0Balance - uint256(amount));
-    //     assertEq(currency1.balanceOf(user), userCurrency1Balance);
-    //   } else {
-    //     assertEq(currency1.balanceOf(user), userCurrency1Balance - uint256(amount));
-    //     assertEq(currency0.balanceOf(user), userCurrency0Balance);
-    //   }
-    //
-    //   if (zeroForOne) {
-    //     assertEq(manager.balanceOf(address(hook), currency0.toId()), balance0Before + uint256(amount));
-    //   } else {
-    //     assertEq(manager.balanceOf(address(hook), currency1.toId()), balance1Before + uint256(amount));
-    //   }
-    //
-    //   assertEq(hook.asyncOrders(poolId, user, zeroForOne), uint256(amount));
-    //
-    //   // Async executor after order
-    //   vm.startPrank(asyncExecutor);
-    //   hook.executeOrder(key, order);
-    //   vm.stopPrank();
-    //
-    //   assertEq(hook.asyncOrders(poolId, user, zeroForOne), 0);
-    //   if (zeroForOne) {
-    //     assertEq(manager.balanceOf(user, currency0.toId()), uint256(amount));
-    //   } else {
-    //     assertEq(manager.balanceOf(user, currency1.toId()), uint256(amount));
-    //   }
+  function testFuzzAsyncSwapOrder(bool zeroForOne, uint256 amount, bool settleUsingBurn) public userAction {
+    vm.assume(amount >= 1);
+    vm.assume(amount <= 1 ether);
+    vm.assume(settleUsingBurn == false);
+
+    uint256 balance0Before = manager.balanceOf(address(hook), currency0.toId());
+    uint256 balance1Before = manager.balanceOf(address(hook), currency0.toId());
+
+    uint256 userCurrency0Balance = currency0.balanceOf(user);
+    uint256 userCurrency1Balance = currency1.balanceOf(user);
+    if (zeroForOne) {
+      token0.approve(address(router), amount);
+    } else {
+      token1.approve(address(router), amount);
+    }
+
+    IAsyncSwap.AsyncOrder memory order = IAsyncSwap.AsyncOrder({
+      key: key,
+      owner: user,
+      zeroForOne: zeroForOne,
+      amountIn: amount,
+      sqrtPrice: 2 ** 96,
+      executor: asyncExecutor
+    });
+
+    router.swap(order, abi.encode(user));
+
+    if (zeroForOne) {
+      assertEq(currency0.balanceOf(user), userCurrency0Balance - uint256(amount));
+      assertEq(currency1.balanceOf(user), userCurrency1Balance);
+    } else {
+      assertEq(currency1.balanceOf(user), userCurrency1Balance - uint256(amount));
+      assertEq(currency0.balanceOf(user), userCurrency0Balance);
+    }
+
+    if (zeroForOne) {
+      assertEq(manager.balanceOf(address(hook), currency0.toId()), balance0Before + uint256(amount));
+    } else {
+      assertEq(manager.balanceOf(address(hook), currency1.toId()), balance1Before + uint256(amount));
+    }
+
+    assertEq(hook.asyncOrders(poolId, user, zeroForOne), uint256(amount));
+
+    // Async executor after order
+    // vm.startPrank(asyncExecutor);
+    // hook.executeOrder(key, order);
+    // vm.stopPrank();
+
+    // assertEq(hook.asyncOrders(poolId, user, zeroForOne), 0);
+    // if (zeroForOne) {
+    //   assertEq(manager.balanceOf(user, currency0.toId()), uint256(amount));
+    // } else {
+    //   assertEq(manager.balanceOf(user, currency1.toId()), uint256(amount));
+    // }
   }
 
 }
