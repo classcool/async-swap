7e7f4d3e20d1c0e0a0dfc4f34c46a334c560f294
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-31 05:30:17 -0500

    chore: clean up deploy scripts

--- start_script.sh
+++ start_script.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 forge script --broadcast -f localhost:8545 --account anvil -vvvv script/00_DeployPoolManager.s.sol --password yes
-
-sleep 3
-
+sleep 2
 forge script --broadcast -f localhost:8545 --account anvil -vvvv script/01_DeployHook.s.sol --password yes
-
-
-sleep 3
-
+sleep 2
 forge script --broadcast -f localhost:8545 --account anvil -vvvv script/02_InitilizePool.s.sol --password yes
-
-sleep 3
-
+sleep 2
 forge script --broadcast -f localhost:8545 --account anvil -vvvv script/03_AddLiquidity.s.sol --password yes
-
-sleep 3
-
+sleep 2
 forge script --broadcast --rpc-url localhost:8545 --account anvil -vvvv script/04_Swap.s.sol --password yes
-
-
-sleep 3
+sleep 2
 forge script --broadcast --rpc-url localhost:8545 --account anvil -vvvv script/05_ExecuteOrder.s.sol --password yes
