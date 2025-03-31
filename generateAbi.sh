f10b90747f032a613af58168dd90c0c578d67666
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-31 08:23:43 -0500

    feat: script to generate abi for typescript file

--- generateAbi.sh
+++ generateAbi.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cast interface AsyncCSMM --json | sed -e '1s/^/export const AsyncCSMMAbi = /' -e '$a\'$'\n'' as const;' > ./packages/indexer/abis/AsyncCSMM.ts
+
+cast interface AsyncCSMM --json | sed -e '1s/^/export const RouterAbi = /' -e '$a\'$'\n'' as const;' > ./packages/indexer/abis/Router.ts
