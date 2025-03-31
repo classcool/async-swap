82b29079cd9bfef3ac0565fa375ed9114abed721
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-31 01:24:41 -0500

    feat: add user params and separate liquidity value provisioning

--- src/interfaces/IAsyncCSMM.sol
+++ src/interfaces/IAsyncCSMM.sol
@@ -9,9 +9,16 @@ interface IAsyncCSMM is IAsyncSwap {
   struct CSMMLiquidityParams {
     PoolKey key;
     address owner;
-    uint256 amountEach;
+    uint256 amount0;
+    uint256 amount1;
+  }
+
+  struct UserParams {
+    address user;
+    address executor;
   }
 
   function addLiquidity(CSMMLiquidityParams calldata liqParams) external;
+  function executeOrder(IAsyncSwap.AsyncOrder calldata order, bytes calldata userParams) external;
 
 }
