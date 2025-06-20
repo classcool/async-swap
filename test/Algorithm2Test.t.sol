be15caf3141db0fa8d603b06ea89be1bdd9b1d72
Meek Msaki <meek10x@gmail.com>
2025-06-20 17:38:32 -0500

    fix misspell name renaming

--- test/Algorithm2Test.t.sol
+++ test/Algorithm2Test.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import { SetupHook } from "./SetupHook.sol";
-import { Algorithm2 } from "@async-swap/aglorithms/algorithm-2.sol";
+import { Algorithm2 } from "@async-swap/algorithms/algorithm-2.sol";
 import { IAlgorithm } from "@async-swap/interfaces/IAlgorithm.sol";
 import "forge-std/Test.sol";
 
