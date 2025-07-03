9c1c4420e5810f866cfd6b8fbd5c6d0c7c349db8
Meek Msaki <meek10x@gmail.com>
2025-07-03 11:56:06 -0500

    fix interface script

--- generateAbi.sh
+++ generateAbi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cast interface AsyncCSMM --json | sed -e '1s/^/export const AsyncCSMMAbi = /' -e '$a\'$'\n'' as const;' > ./packages/indexer/abis/AsyncCSMM.ts
+cast interface AsyncSwapCSMM --json | sed -e '1s/^/export const AsyncSwapCSMMAbi = /' -e '$a\'$'\n'' as const;' > ./packages/indexer/abis/AsyncSwapCSMM.ts
 
 cast interface Router --json | sed -e '1s/^/export const RouterAbi = /' -e '$a\'$'\n'' as const;' > ./packages/indexer/abis/Router.ts
